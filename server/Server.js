59a4aab49acf6d25ba2887bcbb4797da0a66a1f4
jerry <jerry_xguo@yahoo.com>
2018-10-29 11:23:54 +1100

    angular route only

--- server/Server.js
+++ server/Server.js
@@ -8,6 +8,9 @@ route(app);
 // Setting the app router and static folder
 app.use(express.static(path.resolve('../client/dist/flickr')));
 
+app.use(function(req, res) {
+    res.sendFile(path.join(__dirname, '../client/dist/flickr', 'index.html'));
+});
 
 app.listen(3000,function(){
     console.log("Listening on 3000");
